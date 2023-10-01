f04cffe4b69183fa3bd942998d65876531f23fd3
omar1227 <omar.medeiros@hotmail.com>
2023-10-01 00:50:04 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,27 @@
+const botoes = document.querySelectorAll('.botao')
+
+const personagem = document.querySelectorAll(".personagem");
+
+
+botoes.forEach((botoes, index) => {
+    botoes.addEventListener("click", () => {
+
+        desselecionarBotao();
+        desselecionarPersonagem();
+
+        botoes.classList.add('selecionado');
+        personagem[index].classList.add('selecionado');
+    })
+});
+
+const desselecionarBotao = () =>  {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+
+const desselecionarPersonagem = () => {
+    const personagemSelecionado = document.querySelector(".selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+
+}
+
